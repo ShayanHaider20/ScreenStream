b371fca75347f6419725739706312300b00cd710
Shayan Haider <133689579+ShayanHaider20@users.noreply.github.com>
2025-02-21 17:00:24 +0500

    V1

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import "./css/App.css";
+import Home from "./pages/Home";
+import MovieCard from "./components/MovieCard";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/favorites";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
